579669be551332a1818d9678cdc536cdaacde86e
Stephen Steiner <46692873+stephensteiner4@users.noreply.github.com>
2024-07-16 00:56:50 +0000

    begin project; create db; create controllers

---
+++ app/controllers/brush_controller.rb
@@ -0,0 +1,7 @@
+class BrushController
+  def stroke
+
+
+    render({:templates => "home_templates/actor_home"})
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+
+  get("/actors", { :controller => "brush", :action => "stroke"})
+
+  get("/directors", { :controller => "painter", :action => "paint"})
+
+  get("/movies", { :controller => "canvas", :action => "sit"})
 end
